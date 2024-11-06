5d15e73363d9cf6d7760353128bad9b889f71f32
intrusive-rider <intrusive-rider@proton.me>
2024-11-06 14:38:23 +0700

    Code cleanup

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -13,10 +13,6 @@
     ];
 @endphp
 
-{{-- <x-forms.field :$label :$name>
-    <input {{ $attributes($defaults) }}>
-</x-forms.field> --}}
-
 <label for="{{ $name }}" class="input input-bordered flex items-center gap-3">
     <i class="ph-fill ph-{{ $icon }} text-lg"></i>
     <input {{ $attributes($defaults) }} />
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,9 @@
 <body class="antialiased">
     <div class="px-6 space-y-12">
         <x-nav />
-        {{ $slot }}
+        <main class="max-w-[986px] mx-auto space-y-12 pb-12">
+            {{ $slot }}
+        </main>
         <x-footer />
     </div>
 </body>
