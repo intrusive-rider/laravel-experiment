a27bc6d155d69639ce906ee67a8d0a7c6cb04d6a
intrusive-rider <intrusive-rider@proton.me>
2024-11-07 10:41:35 +0700

    Cleaned up code

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,29 +1,3 @@
-{{-- <nav
-    class="navbar flex items-center justify-between bg-base-100 shadow-lg top-0 sticky z-10">
-    <div class="space-x-6 font-bold">
-        <a href="/">
-            <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="" class="inline-block">
-        </a>
-        <a href="/">Jobs</a>
-        <a href="/">Careers</a>
-        <a href="/">Salaries</a>
-        <a href="/">Companies</a>
-    </div>
-    @auth
-        <div class="flex space-x-6 font-bold">
-            @can('create')
-                <a href="/jobs/create">Post a Job</a>
-            @endcan
-            <button type="submit" form="logout">Log out</button>
-        </div>
-    @endauth
-    @guest
-        <a href="/login" class="font-bold">Sign in</a>
-    @endguest
-</nav>
-
-<x-forms.form action="/logout" :method="'DELETE'" class="hidden" id="logout"></x-forms.form> --}}
-
 <div class="navbar bg-base-100">
     <a href="/" class="navbar-start">
         <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
